5515f5f86c2815842e929de2d8ecf54ef31cefd1
Kevin <mr.mcelhinney@gmail.com>
2022-12-29 14:36:30 -0800

    commit tree a6d75fe175ce3f6e2c2c1feda2718aa1e26e7392

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,23 @@
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+    case "counter/decrement":
+      return { count: state.count - 1 };
     default:
       return state;
   }
 }
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
 
-changeState(state, action);
+render();
